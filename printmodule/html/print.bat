e6c78f803a6c9c0878ae54aad17ce99e5a780ab0
琴梨梨 <yeyaohua2010@163.com>
2021-10-04 09:23:32 +0800

    0.1.3 with html support

---
+++ printmodule/html/print.bat
@@ -0,0 +1,10 @@
+@echo on
+echo HTML Print Module 1.0.0
+echo Author:Qinlili
+echo Preparing file: %*
+cd %~dp0
+wkhtmltopdf.exe ..\..\pending\%* temp.pdf
+echo Printing file: %*
+..\pdf\PDFtoPrinter.exe temp.pdf
+del /f /s /q temp.pdf
+exit %ERRORLEVEL%
