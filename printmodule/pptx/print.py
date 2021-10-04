11e0047e2d104f1992536844743ab6cbf4912b00
琴梨梨 <yeyaohua2010@163.com>
2021-10-04 08:27:15 +0800

    0.1.2 pptx support

--- printmodule/pptx/print.py
+++ printmodule/pptx/print.py
@@ -4,6 +4,9 @@ import time
 powerpoint = client.Dispatch("Powerpoint.Application")
 presentation = powerpoint.presentations.Open(os.path.abspath("temp.pptx"))
 presentation.PrintOut()
-time.sleep(1)
+count = 1
+for slide in presentation.Slides:
+    time.sleep(1)
+    count += 1
 presentation.Close()
 powerpoint.Quit()
